82fe23901a42b7338dcbb5d64f1e6304c75b4df8
Renat Shabanov <multizloy@gmail.com>
2024-01-10 19:58:05 +0300

    jump slide

--- pygame_platformer/game.py
+++ pygame_platformer/game.py
@@ -1,9 +1,10 @@
-import pygame, sys
+import pygame, sys, random, math
 
 from scripts.entities import PhysicsEntity, Player
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds, Cloud
+from scripts.particle import Particle
 
 
 class Game:
@@ -13,11 +14,11 @@ class Game:
         pygame.display.set_caption("platformer")
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
-        
+
         self.clock = pygame.time.Clock()
-        
+
         self.movement = [False, False]
-        
+
         self.assets = {
             "decor": load_images("tiles/decor"),
             "grass": load_images("tiles/grass"),
@@ -27,11 +28,12 @@ class Game:
             "background": load_image("background.png"),
             "clouds": load_images("clouds"),
             "player/idle": Animation(load_images("entities/player/idle"), img_dur=6),
-            "player/run": Animation(load_images("entities/player/run"), img_dur=4),
-            "player/jump": Animation(load_images("entities/player/jump"), img_dur=5),
-            "player/slide": Animation(load_images("entities/player/slide"), img_dur=5),
-            "player/wall_slide": Animation(
-                load_images("entities/player/wall_slide"), img_dur=5
+            "player/run": Animation(load_images("entities/player/run")),
+            "player/jump": Animation(load_images("entities/player/jump")),
+            "player/slide": Animation(load_images("entities/player/slide")),
+            "player/wall_slide": Animation(load_images("entities/player/wall_slide")),
+            "particle/leaf": Animation(
+                load_images("particles/leaf"), img_dur=20, loop=False
             ),
         }
 
@@ -42,6 +44,12 @@ class Game:
         self.tilemap = Tilemap(self, tile_size=16)
         self.tilemap.load("map.json")
 
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([("large_decor", 2)], keep=True):
+            self.leaf_spawners.append(
+                pygame.Rect(4 + tree["pos"][0], 4 + tree["pos"][1], 23, 13)
+            )
+        self.particles = []
         self.scroll = [0, 0]
 
     def run(self):
@@ -59,6 +67,22 @@ class Game:
                 - self.scroll[1]
             ) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (
+                        rect.x + random.random() * rect.width,
+                        rect.y + random.random() * rect.height,
+                    )
+                    self.particles.append(
+                        Particle(
+                            self,
+                            "leaf",
+                            pos,
+                            velocity=[-0.1, 0.3],
+                            frame=random.randint(0, 20),
+                        )
+                    )
             # выводим облака
             self.clouds.update()
             self.clouds.render(self.display, offset=render_scroll)
@@ -67,6 +91,14 @@ class Game:
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, offset=render_scroll)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == "leaf":
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -78,7 +110,7 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.player.velocity[1] = -3
+                        self.player.jump()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
--- pygame_platformer/scripts/entities.py
+++ pygame_platformer/scripts/entities.py
@@ -14,6 +14,8 @@ class PhysicsEntity:
         self.anim_offset = (-3, -3)
         self.flip = False
         self.set_action("idle")
+        
+        self.last_movement = [0, 0]
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
@@ -59,6 +61,9 @@ class PhysicsEntity:
             self.flip = False
         if movement[0] < 0:
             self.flip = True
+            
+        self.last_movement = movement
+
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
@@ -85,19 +90,58 @@ class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, "player", pos, size)
         self.air_time = 0
+        self.jumps = 1
+        self.wall_slide = False
 
     def update(self, tilemap, movement=(0, 0)):
         super().update(tilemap, movement=movement)
 
         self.air_time += 1
-
         if self.collisions["down"]:
             self.air_time = 0
+            self.jumps = 1
 
-        if self.air_time > 4:
-            self.set_action("jump")
+        self.wall_slide = False
+        if (self.collisions["right"] or self.collisions["left"]) and self.air_time > 4:
+            self.wall_slide = True
+            self.velocity[1] = min(self.velocity[1], 0.5)
+            if self.collisions["right"]:
+                self.flip = False
+            else:
+                self.flip = True
+            self.set_action("wall_slide")
 
-        elif movement[0] != 0:
-            self.set_action("run")
+        if not self.wall_slide:
+            if self.air_time > 4:
+                self.set_action("jump")
+
+            elif movement[0] != 0:
+                self.set_action("run")
+            else:
+                self.set_action("idle")
+
+        if self.velocity[0]  > 0:
+            self.velocity[0] = max(self.velocity[0] - 0.1, 0)
         else:
-            self.set_action("idle")
+            self.velocity[0] = min(self.velocity[0] + 0.1, 0)
+            
+    def jump(self):
+        if self.wall_slide:
+            if self.flip and self.last_movement[0] < 0:
+                self.velocity[0] = 3.5
+                self.velocity[1] = -2.5
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+            if self.flip and self.last_movement[0] > 0:
+                self.velocity[0] = -3.5
+                self.velocity[1] = -2.5
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+
+        elif self.jumps:
+            self.velocity[1] = -3
+            self.jumps -= 1
+            self.air_time = 5
+            return True
